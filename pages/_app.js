87c590c3b04be4e0d159cc830d681ea3d92dbe96
RissaKo <144802348+RissaKo@users.noreply.github.com>
2023-09-25 13:35:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,12 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       <DefaultSeo
         title="Get Youtube Thumbnail From youtube : Youtube thumbnail downloader tool"
-        description="Download Youtube Thumbnail in high-quality from YouTube videos."
+        description="Download Youtube Thumbnail in high-quality from YouTube videos : If You Looking to get a thumbnail from YouTube? Check out our YouTube thumbnail downloader tool, which allows you to easily download high-quality thumbnails from any YouTube video."
         canonical="https://www.getthumbnailfromyoutube.com"
         openGraph={{
           url: "https://www.getthumbnailfromyoutube.com",
           title: "Get Thumbnail From Youtube",
-          description: "Get Thumbnail From Youtube you can Download high-quality thumbnails from YouTube videos.",
+          description="Download Youtube Thumbnail in high-quality from YouTube videos : If You Looking to get a thumbnail from YouTube? Check out our YouTube thumbnail downloader tool, which allows you to easily download high-quality thumbnails from any YouTube video."
           site_name: "Get Youtube Thumbnail From Youtube",
         }}
       />
